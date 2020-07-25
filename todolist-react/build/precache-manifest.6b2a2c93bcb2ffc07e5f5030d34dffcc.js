0b3797526a9c103637610f73559f81f475aaae14
Sucayono <57749402+CahyaSamudera@users.noreply.github.com>
2020-07-25 21:05:01 +0700

    Add files via upload

---
+++ todolist-react/build/precache-manifest.6b2a2c93bcb2ffc07e5f5030d34dffcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94927f5eab57009f2eef5e6595abfc01",
+    "url": "/React-Todolist/index.html"
+  },
+  {
+    "revision": "49b76e762722c1c1072b",
+    "url": "/React-Todolist/static/css/main.c5bfdfb6.chunk.css"
+  },
+  {
+    "revision": "0e38548a50433bb76fe6",
+    "url": "/React-Todolist/static/js/2.26409650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todolist/static/js/2.26409650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b76e762722c1c1072b",
+    "url": "/React-Todolist/static/js/main.53ff5e89.chunk.js"
+  },
+  {
+    "revision": "da60473f11fa4fd574ca",
+    "url": "/React-Todolist/static/js/runtime-main.f46386fa.js"
+  }
+]);
